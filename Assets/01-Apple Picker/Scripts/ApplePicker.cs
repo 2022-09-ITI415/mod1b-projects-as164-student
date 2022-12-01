ce3087b2bd9b3e328840c798859fa0f084d3ad4d
as164-student <asr164@scarletmail.rutgers.edu>
2022-11-30 21:56:07 -0500

    Basket Collection
    finished basket function

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    
+    public GameObject     basketPreFab;
+    public int            numBaskets = 3;
+    public float          basketBottomY = -14f;
+    public float          basketSpacingY = 2f;
+    
+    
+    
+    
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        for (int i=0; i < 3; i++)
+        {
+
+            GameObject tBasketGO = Instantiate<GameObject>(basketPreFab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+
+        }
+
+
+
+
+
+
     }
 
     // Update is called once per frame
