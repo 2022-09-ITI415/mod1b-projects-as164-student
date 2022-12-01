fc032f83b17d8f226829b919f24e096d7114298d
as164-student <asr164@scarletmail.rutgers.edu>
2022-11-30 20:43:50 -0500

    Movement
    Got tree movement down

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float    speed = 1f;
+    public float    leftAndRightEdge = 10f;
+    public float    chanceToChangeDirections = 0.1f;
+    public float    secondsBetweenAppleDrops = 1f;
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      //dropping apples every second  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       //tree moving and changing direction
+
+       Vector3 pos = transform.position;
+       pos.x += speed * Time.deltaTime;
+       transform.position = pos;
+
+
+       if (pos.x < -leftAndRightEdge){
+
+            speed = Mathf.Abs(speed);
+
+
+
+       } else if (pos.x > leftAndRightEdge) {
+
+
+            speed = -Mathf.Abs(speed);
+
+
+
+       }
+
+
+
+
+
+
     }
 }
